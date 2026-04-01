0ca827c155ab689d1f44ada5c8b3fa9af07d316c
MalikTs <storm00606@gmail.com>
2026-04-01 13:48:30 +0300

    feat: add current timestamp output for v1.1.0

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	fmt.Println("Current time:", time.Now().Format("2006-01-02 15:04:05"))
 }
